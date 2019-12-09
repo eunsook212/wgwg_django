76378c8fb0f9c4a7b89223d5bb4fa4a53684d305
KangEunSook <thezili.eunsook@gmail.com>
2019-12-09 14:13:27 +0900

    boorstrap theme

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.index, name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
